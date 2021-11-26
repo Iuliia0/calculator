c3e520ac067d0e186099342d7883480972a7c4ed
Iuliia Emelianova <julia03951@gmail.com>
2021-11-26 19:00:28 +0300

    Выполнена первая часть задания урока 2

--- main.js
+++ main.js
@@ -1,9 +1,28 @@
-let titel;
-let screens;
-let screenPrice;
-let rollback;
-let fullPrice;
-let adaptive;
+let titel = 'First project';
+let screens = 'Простые, Сложные, Интерактивные';
+let screenPrice = 10000;
+let rollback = 55;
+let fullPrice = 150000;
+let adaptive = true;
 
-alert('Hello!')
-console.log('Hello!')
+let rateOfDollar = 74.60;
+let rateOfHryvia = 0.36;
+let rateOfYuan = 0.36;
+
+console.log(typeof titel);
+console.log(typeof fullPrice);
+console.log(typeof adaptive);
+console.log(screens.length);
+console.log('Стоимость верстки экранов ' + screenPrice + ' рублей.');
+console.log('Стоимость верстки экранов ' + Math.ceil(screenPrice / rateOfDollar) + ' долларов.');
+console.log('Стоимость верстки экранов ' + Math.ceil(screenPrice /  rateOfHryvia) + ' гривен.');
+console.log('Стоимость верстки экранов ' + Math.ceil(screenPrice / rateOfYuan) + ' юани.');
+
+
+console.log('Стоимость разработки сайта' + fullPrice + 'рублей.');
+console.log('Стоимость разработки сайта' + Math.ceil(fullPrice / rateOfDollar) + 'долларов.');
+console.log('Стоимость разработки сайта' + Math.ceil(fullPrice /  rateOfHryvia) + 'гривен.');
+console.log('Стоимость разработки сайта' + Math.ceil(fullPrice / rateOfYuan) + 'юани.');
+
+console.log(screens.toLowerCase().split())
+console.log(fullPrice * (rollback/100))
