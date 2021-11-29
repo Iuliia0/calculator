cb32f69c4bdfd2c8683757e81533b38a83c81841
Iuliia Emelianova <julia03951@gmail.com>
2021-11-29 14:48:04 +0300

    Выполнен 3 урок

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-let titel = 'First project';
+let title = 'First project';
 let screens = 'Простые, Сложные, Интерактивные';
 let screenPrice = 10000;
 let rollback = 55;
@@ -9,7 +9,7 @@ let rateOfDollar = 74.60;
 let rateOfHryvia = 0.36;
 let rateOfYuan = 0.36;
 
-console.log(typeof titel);
+console.log(typeof title);
 console.log(typeof fullPrice);
 console.log(typeof adaptive);
 console.log(screens.length);
@@ -26,3 +26,48 @@ console.log('Стоимость разработки сайта' + Math.ceil(ful
 
 console.log(screens.toLowerCase().split())
 console.log(fullPrice * (rollback/100))
+
+title = prompt('Как называется Ваш проект?')
+console.log('Проект называется: ' + title)
+
+screens = prompt('Какие типы экранов нужно разработать?')
+console.log('Нужны экраны: ' + screens)
+
+screenPrice  = +prompt('Сколько будет стоить данная работа?')
+console.log('Данная работа будет стоить: ' + screenPrice)
+
+adaptive = confirm('Нужен ли адаптив на сайте?')
+console.log(adaptive)
+
+// альтернативный вариант
+// adaptive = ('Нужен ли адаптив на сайте?') === 'Да' || 'да' ? true : false
+// console.log(adaptive)
+
+let service1 = prompt('Какой дополнительный тип услуги нужен?')
+console.log('Нужно: ' + service1)
+
+let servicePrice1 = +prompt('Сколько это будет стоить?')
+console.log(service1 + ' будет стоить: ' + servicePrice1)
+
+let service2 = prompt('Какой дополнительный тип услуги нужен?')
+console.log('Нужно: ' + service2)
+
+let servicePrice2 = +prompt('Сколько это будет стоить?')
+console.log(service2 + ' будет стоить: ' + servicePrice2)
+
+fullPrice = screenPrice + servicePrice1 + servicePrice2;
+console.log('Итоговая стоимость: ' + fullPrice)
+
+let servicePercentPrice = Math.ceil(fullPrice - (fullPrice * (rollback/100)))
+console.log('Итоговая стоимость после отката посреднику: ' + servicePercentPrice)
+
+
+if (fullPrice  >= 30000) {
+  console.log('Даем скидку в 10%')
+} else if (fullPrice  >= 15000 && fullPrice  < 30000) {
+  console.log('Даем скидку в 5%')
+} else if (fullPrice < 15000 && fullPrice >= 0) {
+  console.log('Скидка не предусмотрена')
+} else if (fullPrice < 0) {
+  console.log('Что-то пошло не так')
+}
