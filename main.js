dd9ca611543d649a2fca58e0ce7291d5f4f749bf
Iuliia Emelianova <julia03951@gmail.com>
2021-11-30 21:33:32 +0300

    Выполнено задание 4 урока

--- main.js
+++ main.js
@@ -1,46 +1,65 @@
 'use strict'
 
-let title = prompt('Как называется Ваш проект?');
-let screens = prompt('Какие типы экранов нужно разработать?');
-let screenPrice = +prompt('Сколько будет стоить данная работа?');
+let title = getTitlel(prompt('Как называется Ваш проект?'));
+let screens = prompt('Какие типы экранов нужно разработать?').split(' ');
+let screenPrice =  +prompt('Сколько будет стоить данная работа?');
 let adaptive = confirm('Нужен ли адаптив на сайте?');
-let rollback = 10;
 let service1 = prompt('Какой дополнительный тип услуги нужен?');
 let servicePrice1 = +prompt('Сколько это будет стоить?');
 let service2 = prompt('Какой дополнительный тип услуги нужен?');
-let servicePrice2 = +prompt('Сколько это будет стоить?');
-let fullPrice = screenPrice + servicePrice1 + servicePrice2;
-let servicePercentPrice = Math.ceil(fullPrice - (fullPrice * (rollback/100)))
+let servicePrice2 =  +prompt('Сколько это будет стоить?');
 
-let rateOfDollar = 74.60;
-let rateOfHryvia = 0.36;
-let rateOfYuan = 0.36;
+let rollback = 10;
 
-if (fullPrice  >= 30000) {
-  console.log('Даем скидку в 10%')
-} else if (fullPrice  >= 15000 && fullPrice  < 30000) {
-  console.log('Даем скидку в 5%')
-} else if (fullPrice < 15000 && fullPrice >= 0) {
-  console.log('Скидка не предусмотрена')
-} else if (fullPrice < 0) {
-  console.log('Что-то пошло не так')
+let allServicePrices, fullPrice, servicePercentPrice
+
+const showTypeOf = function (variable) {
+  console.log(variable, typeof variable)
 }
 
-console.log(typeof title);
-console.log(typeof fullPrice);
-console.log(typeof adaptive);
-console.log(screens.length);
-console.log('Стоимость верстки экранов ' + screenPrice + ' рублей.');
-console.log('Стоимость верстки экранов ' + Math.ceil(screenPrice / rateOfDollar) + ' долларов.');
-console.log('Стоимость верстки экранов ' + Math.ceil(screenPrice /  rateOfHryvia) + ' гривен.');
-console.log('Стоимость верстки экранов ' + Math.ceil(screenPrice / rateOfYuan) + ' юани.');
+const getAllServicePrices = function (price1, price2) {
+  return price1 + price2
+}
 
-console.log('Стоимость разработки сайта' + fullPrice + 'рублей.');
-console.log('Стоимость разработки сайта' + Math.ceil(fullPrice / rateOfDollar) + 'долларов.');
-console.log('Стоимость разработки сайта' + Math.ceil(fullPrice /  rateOfHryvia) + 'гривен.');
-console.log('Стоимость разработки сайта' + Math.ceil(fullPrice / rateOfYuan) + 'юани.');
+function getFullPrice (allPrice, price) {
+  return allPrice + price
+}
 
-console.log(screens.toLowerCase().split())
-console.log(fullPrice * (rollback/100))
+const getRollbackMessage = function (price) {
+    if (price  >= 30000) {
+    return 'Даем скидку в 10%'
+  } else if (price  >= 15000 && price  < 30000) {
+    return 'Даем скидку в 5%'
+  } else if (price < 15000 && price >= 0) {
+    return 'Скидка не предусмотрена'
+  } else if (price < 0) {
+    return 'Что-то пошло не так'
+  }
+}
 
-console.log('Итоговая стоимость после отката посреднику: ' + servicePercentPrice)
+  function getTitlel (str) {
+  let arrTitle = str.split(' ').filter(arr => arr !== '');
+  let newTitle = ''
+  for (let i = 0; i <= arrTitle.length-1; i++) {
+    newTitle += arrTitle[i] + ' '
+  }
+  newTitle = newTitle[0].toUpperCase() + newTitle.slice(1)
+  return newTitle
+}
+
+const getServicePercentPrices = function (allPrice, percent) {
+  return Math.ceil(allPrice - (allPrice * (percent/100)));
+}
+
+allServicePrices = getAllServicePrices(servicePrice1, servicePrice2);
+fullPrice = getFullPrice(allServicePrices, screenPrice);
+servicePercentPrice = getServicePercentPrices(fullPrice, rollback)
+
+showTypeOf(title);
+showTypeOf(fullPrice);
+showTypeOf(adaptive);
+
+
+console.log('Стоимость за вычетом процента отката посреднику ' + servicePercentPrice); 
+console.log(getRollbackMessage(fullPrice))
+console.log(screens);
